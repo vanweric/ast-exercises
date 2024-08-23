48b3d139749dca9c283f895386a73ed4fed8413c
Eric VanWyk <vanweric+github@gmail.com>
2024-08-23 09:14:08 -0700

    Fixed codemirror scrolling

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import {
   Card,
   Accordion,
   Button,
+  Tabs,
+  Tab,
+  NavDropdown,
 } from "react-bootstrap";
 import CodeMirror from "@uiw/react-codemirror";
 import { basicSetup, EditorView } from "@uiw/react-codemirror";
@@ -19,56 +22,51 @@ import {
 } from "@codemirror/lang-python";
 import { LanguageSupport } from "@codemirror/language";
 import * as themes from "@uiw/codemirror-themes-all";
-import { useAccordionButton } from 'react-bootstrap/AccordionButton';
-
+import { useAccordionButton } from "react-bootstrap/AccordionButton";
 
 import "./App.css";
 import fakeresults from "./fakeresults.json";
 
-
-
-
+import usePyodide from "./usePyodide";
 
 const UnitTestResultsList = ({ results }) => {
   return (
     <Accordion>
       {results.map((test, index) => (
-        <div className={test.success? 'accordion-success':'accordion-fail'}>
-        <Accordion.Item eventKey={index} style={{ backgroundColor: test.success ? 'lightgreen' : 'tomato', color: 'black' }}>
-          <Accordion.Header >
-            {test.name}
-          </Accordion.Header>
-          <Accordion.Body>
-            <div>
-              <strong>Expected Response:</strong> {test.expectedResponse}
-            </div>
-            <div>
-              <strong>Received Response:</strong> {test.receivedResponse}
-            </div>
-          </Accordion.Body>
-        </Accordion.Item>
+        <div className={test.success ? "accordion-success" : "accordion-fail"}>
+          <Accordion.Item
+            eventKey={index}
+            style={{
+              backgroundColor: test.success ? "lightgreen" : "tomato",
+              color: "black",
+            }}
+          >
+            <Accordion.Header>{test.name}</Accordion.Header>
+            <Accordion.Body>
+              <div>
+                <strong>Expected Response:</strong> {test.expectedResponse}
+              </div>
+              <div>
+                <strong>Received Response:</strong> {test.receivedResponse}
+              </div>
+            </Accordion.Body>
+          </Accordion.Item>
         </div>
       ))}
     </Accordion>
   );
 };
 
-
 const TitleBar = () => {
   return (
     <Navbar expand="md" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Python Desynced Crosscompiler</Navbar.Brand>
+        <Navbar.Brand href="#home">AST Exercises</Navbar.Brand>
 
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link
-              href="https://stagegames.github.io/DesyncedJavaScriptUtils/"
-              target="_blank"
-            >
-              Disassembler
-            </Nav.Link>
+
             <Nav.Link
               href="https://github.com/vanweric/PythonToDesyncedCompiler/blob/main/public/Info.md"
               target="_blank"
@@ -89,7 +87,16 @@ const TitleBar = () => {
             </Nav.Link>
           </Nav>
 
-          <Nav className="ms-auto">A</Nav>
+          <Nav className="ms-auto">
+            <NavDropdown title="Exercises"
+            id="basic-nav-dropdown"
+            >
+
+
+            </NavDropdown>
+
+
+          </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
@@ -98,8 +105,7 @@ const TitleBar = () => {
 
 const CodeBlock = () => {
   return (
-    <Container fluid="xl" className="flex-grow-1 position-relative">
-      <div classNamse="p-4 bg-white border rounded shadow-sm position-absolute top-0 bottom-0 start-0 end-0 overflow-auto">
+
         <CodeMirror
           className="cm-outer-container"
           align="left"
@@ -115,12 +121,17 @@ const CodeBlock = () => {
             EditorView.lineWrapping,
           ]}
         />
-      </div>
-    </Container>
   );
 };
 
 function App() {
+  const { pyodide, loading } = usePyodide();
+
+  const loadExample = async (name) => {
+
+  };
+
+
   return (
     <div className="App">
       <div className="d-flex flex-column vh-100">
@@ -128,22 +139,49 @@ function App() {
         <TitleBar />
 
         {/* Main content */}
-          <Container fluid="xl" className="flex-grow-1 position-relative">
-          <div className="p-4 bg-white position-absolute top-0 bottom-0 start-0 end-0 overflow-auto">
-            <Row >
-              <Col >
-                <CodeBlock />
-              </Col>
-              <Col className="bg-secondary text-white p-3">
-                <UnitTestResultsList results={fakeresults} />
+        <Container
+          fluid="xl"
+          className="flex-grow-1 position-relative bg-light"
+          style={{ height: "100vh" }}
+        >
+          <Row className="h-100">
+            <Col
+              style={{
+                position: "absolute",
+                height: "100%",
+                width: "64%",
+                overflowY: 'auto'
 
-                <h3>Right Column</h3>
-
-                blah blah blah
-              </Col>
-            </Row>
-          </div>
-          </Container>
+              }}
+            >
+              <br></br>
+              <CodeBlock />
+            </Col>
+            <Col
+              className="bg-secondary"
+              style={{
+                position: "absolute",
+                height: "100%",
+                width: "32%",
+                right: 0,
+              }}
+            >
+              <Tabs
+                defaultActiveKey="description"
+                id="side-column"
+                className="mb-3"
+              >
+                <Tab eventKey="description" title="Description">
+                  <h3>Right Column</h3>
+                  blah blah blah
+                </Tab>
+                <Tab eventKey="tests" title="Tests">
+                  <UnitTestResultsList results={fakeresults} />
+                </Tab>
+              </Tabs>
+            </Col>
+          </Row>
+        </Container>
         {/* Footer */}
 
         <footer className="bg-light py-3 mt-auto">
