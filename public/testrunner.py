57084d96349068ca5e4a1cbe5696a6418f981555
Eric VanWyk <vanweric+github@gmail.com>
2024-08-27 14:56:59 -0700

    TestRunner beginning to support types

--- public/testrunner.py
+++ public/testrunner.py
@@ -21,14 +21,37 @@ output format
 '''
 import json
 
+
+
 def runtests(func, inputjson):
-    print("func", func)
     try:
         exec(func)
     except Exception as e:
+        print("An Error Occurred while processing the input function:")
         print(e)
-        return json.dumps({success: False})
-    print("OH YEAH")
-    print("inputjson", inputjson)
-    print("func", func)
-    return json.dumps({success:True, results: []})
+        return json.dumps({'success': False})
+
+    if inputjson['functionName'] in locals():
+         functor = locals()[inputjson['functionName']]
+    else:
+        print("Function Not Found")
+        return json.dumps({'success':False})
+    
+    results=[]
+    for test in inputjson['tests']:
+        result = {}
+        result['name'] = test['name']
+        result['expectedResponse'] = test['expectedResult']
+        try:
+            output = functor(test['input'])
+        except Exception as e:
+            output = str(type(e))
+        result['input'] = test['input']
+        result['receivedResponse'] = output
+        result['success'] = output == test['expectedResult']
+
+        results.append(result)
+
+
+
+    return json.dumps({'success':True, 'results': results})
